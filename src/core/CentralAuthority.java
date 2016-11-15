6cf1be62955c17b26ce58153fa2e13c26dd37d35
gungoel <gunjan.goel100@gmail.com>
2016-11-16 03:36:18 +0530

    Central Authority file added

---
+++ src/core/CentralAuthority.java
@@ -0,0 +1,35 @@
+package core;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class CentralAuthority {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CentralAuthority c=new CentralAuthority();
+		KeyPair k=c.generateKeyPair();
+		System.out.println("Private key "+k.getPrivate());
+		System.out.println("Public Key "+k.getPublic());
+
+	}
+
+	public KeyPair generateKeyPair()
+	{
+		KeyPair keys=null;
+		try {
+			 keys = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return keys;
+		
+		//Cipher cipher = Cipher.getInstance("RSA");
+		
+
+	}
+
+}
